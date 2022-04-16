d81e1f14ff946d860e78bc8326c5e5d3bd8ed50a
CorpadeanDariusCiprian <dcorpodean@gmail.com>
2022-04-16 20:09:10 +0300

    lab8-without test

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -14,7 +14,7 @@ void displayMenu() {
 	cout << "S|s  show all offers" << endl;
 	cout << "P|p  filter offers by price" << endl;
 	cout << "T|t  filter offers by type" << endl;
-	cout << "B|b  filter offers by type and price" << endl;
+	cout << "B|b  filter by any 2 criteria" << endl;
 	cout << "Q|q  quit" << endl;
 }
 
@@ -23,7 +23,7 @@ int main() {
 	//OfferTest::runAllTests();
 	//FilteringTest::runAllTests();
 	//DynamicArrayTest::runAllTests();
-	Offer ofer1;
+	/*Offer ofer1;
 	ofer1.setDeparture("Bucharest");
 	ofer1.setDestination("Rome");
 	ofer1.setDataStart("01/01/2023");
@@ -41,16 +41,13 @@ int main() {
 	arr.append(ofer2);
 	result = z.filter(arr);
 	cout << result;
+	*/
+
 	
-
-
-
-	}
-	/*
 	DynamicArray<Offer> arr;
-	arr.append(ofer1);
+	//arr.append(ofer1);
 	string choice;
-	choice = 'p';
+	choice = "p";
 	while (choice != "q" || choice != "Q")
 	{
 		displayMenu();
@@ -102,9 +99,9 @@ int main() {
 		{
 			DynamicArray<Offer> result;
 			cout << "Price: ";
-			int price;
+			float price;
 			cin >> price;
-			FilterPrice p(price);
+			FilterPrice<Offer> p(price);
 			result = p.filter(arr);
 			cout << result;
 		}
@@ -131,17 +128,135 @@ int main() {
 			{
 				type = CIRCUIT;
 			}
-			FilterType t(type);
+			FilterOfferType<Offer> t(type);
 			result = t.filter(arr);
 			cout << result << endl;
 		}
 		else if (choice == "b" || choice == "B")
 		{
 			DynamicArray<Offer> result;
-			int ty, price;
-			OfferType type;
+			cout << "Choose Criteria 1: "<<endl;
+			cout << "1-Price; 2-Departure 3-Destination; 4-Type" << endl;
+			int choice1, choice2;
+			cin >> choice1;
+			cout << "Choose Criteria 2: " << endl;
+			cout << "1-Price; 2-Departure 3-Destination; 4-Type" << endl;
+			cin >> choice2;
+			if (choice1 == 1 && choice2 == 1) {
+				DynamicArray<Offer> result;
+				float price1, price2;
+				cout << "Price1:"; cin >> price1;
+				cout << "Price2:"; cin >> price2;
+				FilteringCriteriaAnd<Offer, FilterPrice<Offer>, FilterPrice<Offer>, float, float> z(price1, price2);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if((choice1==1&& choice2==2)||(choice1==2&& choice2==1))
+			{ 
+				DynamicArray<Offer> result;
+				float price; string departure;
+				cout << "Price:"; cin >> price;
+				cout << "Departure:"; cin >> departure;
+				FilteringCriteriaAnd<Offer, FilterPrice<Offer>, FilterDeparture<Offer>, float, string> z(price, departure);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if ((((choice1 == 1 && choice2 == 3) || (choice1 == 3 && choice2 == 1)))) {
+				DynamicArray<Offer> result;
+				float price; string destination;
+				cout << "Price:"; cin >> price;
+				cout << "Destination: "; cin >> destination;
+				FilteringCriteriaAnd<Offer, FilterPrice<Offer>, FilterDestination<Offer>, float, string> z(price, destination);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if ((((choice1 == 1 && choice2 == 4) || (choice1 == 4 && choice2 == 1)))) {
+				DynamicArray<Offer> result;
+				float price;
+				float ty; OfferType  type;
+				cout << "Price:"; cin >> price;
+				cout << "Type:0-Circuit,1-CityBreak,2-Cruise,3-AllInclusive\n";
+				cin >> ty;
+				if (ty == 1)
+				{
+					type = CITYBREAK;
+				}
+				else if (ty == 2)
+				{
+					type = CRUISE;
+				}
+				else if (ty == 3)
+				{
+					type = ALLINCLUSIVE;
+				}
+				else if (ty == 0)
+				{
+					type = CIRCUIT;
+				}
+				FilteringCriteriaAnd < Offer, FilterPrice<Offer>, FilterOfferType<Offer>, float, OfferType> z(price, type);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if (choice1 == 2 && choice2 == 2) {
+				DynamicArray<Offer> result;
+				string dep1, dep2;
+				cout << "Departure1:"; cin >> dep1;
+				cout << "Departure2:"; cin >> dep2;
+				FilteringCriteriaAnd<Offer, FilterDeparture<Offer>, FilterDeparture<Offer>, string, string> z(dep1,dep2);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if ((choice1 == 2 && choice2 == 3)|| (choice1 == 3 && choice2 == 2)) {
+				DynamicArray<Offer> result;
+				string dep1, dep2;
+				cout << "Departure:"; cin >> dep1;
+				cout << "Destiantion:"; cin >> dep2;
+				FilteringCriteriaAnd<Offer, FilterDeparture<Offer>, FilterDestination<Offer>, string, string> z(dep1, dep2);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if ((((choice1 == 2 && choice2 == 4) || (choice1 == 4 && choice2 == 2)))) {
+				DynamicArray<Offer> result;
+				string departure;
+				float ty; OfferType type;
+				cout << "Departure:"; cin >> departure;
+				cout << "Type:0-Circuit,1-CityBreak,2-Cruise,3-AllInclusive\n";
+				cin >> ty;
+				if (ty == 1)
+				{
+					type = CITYBREAK;
+				}
+				else if (ty == 2)
+				{
+					type = CRUISE;
+				}
+				else if (ty == 3)
+				{
+					type = ALLINCLUSIVE;
+				}
+				else if (ty == 0)
+				{
+					type = CIRCUIT;
+				}
+				FilteringCriteriaAnd < Offer, FilterDeparture<Offer>, FilterOfferType<Offer>, string, OfferType> z(departure, type);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if (choice1 == 3 && choice2 == 3)  {
+				DynamicArray<Offer> result;
+				string dep1, dep2;
+				cout << "Destination:"; cin >> dep1;
+				cout << "Destiantion:"; cin >> dep2;
+				FilteringCriteriaAnd<Offer, FilterDestination<Offer>, FilterDestination<Offer>, string, string> z(dep1, dep2);
+				result = z.filter(arr);
+				cout << result;
+			}
+			else if ((((choice1 == 3 && choice2 == 4) || (choice1 == 4 && choice2 ==3 )))) {
+			DynamicArray<Offer> result;
+			string departure;
+			float ty; OfferType type;
+			cout << "Departure:"; cin >> departure;
 			cout << "Type:0-Circuit,1-CityBreak,2-Cruise,3-AllInclusive\n";
-
 			cin >> ty;
 			if (ty == 1)
 			{
@@ -159,11 +274,57 @@ int main() {
 			{
 				type = CIRCUIT;
 			}
-			cout << "Price";
-			cin >> price;
-			FilterTypeAndPrice F(price, type);
-			result = F.filter(arr);
-			cout << result << endl;
+			FilteringCriteriaAnd < Offer, FilterDestination<Offer>, FilterOfferType<Offer>, string, OfferType> z(departure, type);
+			result = z.filter(arr);
+			cout << result;
+			}
+			else if (choice1 == 4 && choice2 == 4) {
+			DynamicArray<Offer> result;
+			float ty1,ty2; OfferType type1,type2;
+			cout << "Type:0-Circuit,1-CityBreak,2-Cruise,3-AllInclusive\n";
+			cout << "Type1:"; cin >> ty1;
+			cout << "Type:0-Circuit,1-CityBreak,2-Cruise,3-AllInclusive\n";
+			cout << "Type2:"; cin >> ty2;
+			if (ty1 == 1)
+			{
+				type1 = CITYBREAK;
+			}
+			else if (ty1 == 2)
+			{
+				type1 = CRUISE;
+			}
+			else if (ty1 == 3)
+			{
+				type1 = ALLINCLUSIVE;
+			}
+			else if (ty1 == 0)
+			{
+				type1 = CIRCUIT;
+			}
+			if (ty2 == 1)
+			{
+				type2 = CITYBREAK;
+			}
+			else if (ty2 == 2)
+			{
+				type2 = CRUISE;
+			}
+			else if (ty2 == 3)
+			{
+				type2 = ALLINCLUSIVE;
+			}
+			else if (ty2 == 0)
+			{
+				type2 = CIRCUIT;
+			}
+			FilteringCriteriaAnd < Offer, FilterOfferType<Offer>, FilterOfferType<Offer>, OfferType, OfferType> z(type1, type2);
+			result = z.filter(arr);
+			cout << result;
+			}
+
+			//FilterCriteriaAnd<Offer, FilterPrice<Offer>, FilterDeparture<Offer>, float, string> z(2000.0, "Bucharest");
+			//result = F.filter(arr);
+			//cout << result << endl;
 		}
 		else if (choice == "q" || choice == "Q")
 		{
@@ -173,14 +334,7 @@ int main() {
 	return 0;
 
 
-	///arr.append("DynamicArray class");
-	//arr.append("NOT");
-	///arr.append("tested");
-
-	///cout << arr << endl;
-
 	displayMenu();
 
 	return 0;
 }
-*/
